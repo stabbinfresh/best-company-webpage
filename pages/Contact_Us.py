81f2e963fdd6f745d305b28592a34451e181a700
Andrew Kuhs <andythekuhs@gmail.com>
2023-03-01 23:26:08 -0500

    select box update

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,16 +1,23 @@
 import streamlit as st
 from send_email import send_email
+import pandas as pd
 
 # st.header("Contact Me")
 
+df = pd.read_csv("topics.csv")
+
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
-    raw_subject = st.text_input("What topic do you want to discuss?")
+    raw_subject = st.selectbox(
+        'What topic do you want to discuss?',
+        df["topic"]
+    )
     raw_message = st.text_area("Text")
     message = f"""\
 Subject: New email from {user_email}
 
-From: {user_email} about {raw_subject}
+From: {user_email} 
+Topic {raw_subject}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
