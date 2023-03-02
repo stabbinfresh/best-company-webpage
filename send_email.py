aea3b8656c1b21a953d11cd64f7b34e4d1d42579
Andrew Kuhs <andythekuhs@gmail.com>
2023-03-02 00:02:21 -0500

    added secure email

--- send_email.py
+++ send_email.py
@@ -1,14 +1,17 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "andythekuhs@gmail.com"
-    password = "ghyqukmnpvuwmmmw"
+    password = os.getenv("PASSWORD")
     receiver = "andythekuhs@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+
+
+x = "ghyqukmnpvuwmmmw"
